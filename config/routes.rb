b0ccc81871c8fe1f96af77504978facc244e37df
Zack Ryan <zackryan@zacks-mbp.home>
2013-11-11 01:05:30 -0500

    fixes CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Sueblog::Application.routes.draw do
   resources :posts
 
-  root :to => "posts#index"
+  root :to => "posts#construction"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
